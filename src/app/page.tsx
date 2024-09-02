64b7c5cff7fc8b33e326958c3fb53c5a2cd3121b
Robert Luiten <156024622+RobertLuiten@users.noreply.github.com>
2024-09-01 22:48:40 -0700

    Made fire homepage

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React, { useEffect, useState } from "react";
+
+/**
+ * Creates the homepage of the website!
+ * @returns The homepage of the website!
+ */
+export default function Home() {
+  return(
+    <div>RobertLuiten.com</div>
+  );
+
+}
