a35cc77a6aa79906005d5044dfb730a8191aedd3
Robert Luiten <156024622+RobertLuiten@users.noreply.github.com>
2024-09-09 12:58:10 -0700

    Fixed Vercel issue

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -11,22 +11,22 @@ export default function About() {
     <br></br>
     <h1 className="font-bold text-3xl">The Story Of Robert Luiten</h1>
     <br></br>
-    <img id="Robert's Senior Photo" src="/images/senior_photo.png" alt="Home" style={{ width: '200px'}} className="rounded-xl"/>
+    <img id="Robert&apos;s Senior Photo" src="/images/senior_photo.png" alt="Home" style={{ width: '200px'}} className="rounded-xl"/>
     <br></br>
     <h1 className="font-bold text-xl">Whomever is Robert Luiten Perchance?</h1>
     <br></br>
     <p>Robert Luiten is perhaps the greatest mind of this generation. Considered by many to be the modern day contemporary
-      of Socrates, DaVinci, and Abraham Lincoln, Robert has inspired awe and wonder into all of those he meets. He's really
-      smart and handsome and rich and everyone loves him. And that's on god fr fr.
+      of Socrates&#44; DaVinci&#44; and Abraham Lincoln&#44; Robert has inspired awe and wonder into all of those he meets. He&apos;s really
+      smart and handsome and rich and everyone loves him. And that&apos;s on god fr fr.
     </p>
     <br></br>
     <p>But how did he become the man he is today?</p>
     <br></br>
     <h1 className="font-bold text-xl">Early Life</h1>
     <br></br>
-    <p>Robert was born in Sussex, Britian on October 14th, 1991 to his loving mother Queen Elizabeth II and her secret lover,
-      Scrimble The Love Wizard. Unfortunately, due to the circumstances of which Robert was conceived, and the position that
-      his mother held, Queen Elizabeth II abondoned her son and left him to be raised by the dastardly Scrimble The Love Wizard.
+    <p>Robert was born in Sussex&#44; Britian on October 14th&#44; 1991 to his loving mother Queen Elizabeth II and her secret lover&#44;
+      Scrimble The Love Wizard. Unfortunately&#44; due to the circumstances of which Robert was conceived&#44; and the position that
+      his mother held&#44; Queen Elizabeth II abondoned her son and left him to be raised by the dastardly Scrimble The Love Wizard.
       Scrimble The Love Wizard and Robert did some crazy stuff or something I think they made a flying car.
     </p>
     <br></br>
@@ -38,12 +38,12 @@ export default function About() {
     <br></br>
     <h1 className="font-bold text-xl">Robert Get Leg Extensions</h1>
     <br></br>
-    <p>At the age of 16, Robert got leg extnesions. This added 4 1/2 inches onto his height.</p>
+    <p>At the age of 16&#44; Robert got leg extnesions. This added 4 1/2 inches onto his height.</p>
     <br></br>
     <h1 className="font-bold text-xl">Zeta Iota Gamma Mu Alpha Sigma Presidency</h1>
     <br></br>
-    <p>When Robert was admitted to the University of Washington, he immedetely joined the Zeta Iota Gamma Mu Alpha Sigma fraternity, 
-      and currently serves as the President. "I'm Robert Luiten", he has said, "I'm Robert Luiten from RobertLuiten.com". And we couldn't 
+    <p>When Robert was admitted to the University of Washington&#44; he immedetely joined the Zeta Iota Gamma Mu Alpha Sigma fraternity&#44; 
+      and currently serves as the President. "I&apos;m Robert Luiten"&#44; he has said&#44; &quot;I&apos;m Robert Luiten from RobertLuiten.com&quot;. And we couldn&apos;t 
       agree more.
     </p>
     <br></br>
